7d320d0ceeaec49f99892b7ebc7cfd40c2fa8688
Nagarjuna Immadi <nagarjuna.immadi@real-digital.de>
2026-01-18 11:34:10 +0100

    Enhance flexible column layout with full-screen navigation and UI state management; add About page view

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -3,9 +3,8 @@ sap.ui.define([
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	"sap/ui/model/Sorter",
-	"sap/m/MessageBox",
-	"sap/f/library"
-], function (Controller, Filter, FilterOperator, Sorter, MessageBox, fioriLibrary) {
+	"sap/m/MessageBox"
+], function (Controller, Filter, FilterOperator, Sorter, MessageBox) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.fcl.controller.Master", {
@@ -41,9 +40,15 @@ sap.ui.define([
 
 		onListItemPress: function (oEvent) {
 			var productPath = oEvent.getSource().getBindingContext("products").getPath(),
-				product = productPath.split("/").slice(-1).pop();
-
-			this.oRouter.navTo("detail", {layout: fioriLibrary.LayoutType.TwoColumnsMidExpanded, product: product});
+				product = productPath.split("/").slice(-1).pop(),
+				oNextUIState;
+			this.getOwnerComponent().getHelper().then(function (oHelper) {
+				oNextUIState = oHelper.getNextUIState(1);
+				this.oRouter.navTo("detail", {
+					layout: oNextUIState.layout,
+					product: product
+				});
+			}.bind(this));
 		}
 	});
 });
